43eed992e45d7403f0f3d0489e8e07984cb2b2cf
Viktor Eikman <viktor.eikman@gmail.com>
2019-01-22 19:48:47 +0100

    Cosmetics.

--- src/dmote_keycap/models.clj
+++ src/dmote_keycap/models.clj
@@ -4,6 +4,10 @@
   (:require [scad-clj.model :as model]
             [scad-tarmi.dfm :refer [error-fn]]))
 
+;;;;;;;;;;;;;;;
+;; Constants ;;
+;;;;;;;;;;;;;;;
+
 (def matias
   {:travel 3.5
    :stem {:x 4.5
@@ -18,16 +22,20 @@
 (def wall-thickness 1)
 (def scale 0.9)
 
+;;;;;;;;;;;;;;
+;; Internal ;;
+;;;;;;;;;;;;;;
+
 (def compensator-general (error-fn))
 (def compensator-positive (error-fn 0.5))
 
-(defn rounded
+(defn- rounded
   [[x y] radius]
   (let [initial #(- % radius)]
     (->> (model/square (initial x) (initial y))
          (model/offset radius))))
 
-(defn outer-body
+(defn- outer-body
   [footprint]
   (let [xy (map #(+ % wall-thickness) footprint)]
     (model/hull
@@ -39,14 +47,14 @@
         {:height 1 :center false}
         (rounded xy 2)))))
 
-(defn inner-body
+(defn- inner-body
   [footprint]
   (let [xy (map compensator-general footprint)]
     (model/extrude-linear
       {:height (get-in matias [:body :top :z]), :center false, :scale scale}
       (rounded xy 0.6))))
 
-(defn composite-body
+(defn- smallflat-composite-body
   []
   (let [footprint [(get-in matias [:body :top :x])
                    (get-in matias [:body :top :y])]]
@@ -55,7 +63,7 @@
         (outer-body footprint)
         (inner-body footprint)))))
 
-(defn stem
+(defn- stem
   [options]
   (let [z (get-in matias [:stem :z :interior])]
     (model/translate [0 0 (- z)]
@@ -65,9 +73,13 @@
                   (compensator-positive (get-in matias [:stem :y]))]
                  0.4)))))
 
+;;;;;;;;;;;;;;;
+;; Interface ;;
+;;;;;;;;;;;;;;;
+
 (defn smallflat-model
   "A flat keycap that barely covers the uppermost part of a Matias switch."
   [options]
   (model/union
-    (composite-body)
+    (smallflat-composite-body)
     (stem options)))
